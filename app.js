abccf8a0c6639fba7a1c27d73d7590435af217dd
Ruben Sargsyan <rubenswall@gmail.com>
2016-10-26 04:13:37 +0400

    i

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session = require('express-session');
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/comics', function(err) {
+mongoose.connect('mongodb://comics-admin:born77villain77ml77c@ds031597.mlab.com:31597/comics', function(err) {
     if (err) {
         console.err(err);
     } else {
